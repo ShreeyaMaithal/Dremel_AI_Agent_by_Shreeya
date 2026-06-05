d71af21abda7c10585bfdca045f8261e4f598991
ShreeyaMaithal <mshreeya.work@gmail.com>
2026-06-05 12:55:24 +0530

    Update dashboard_app.py

--- dashboard/dashboard_app.py
+++ dashboard/dashboard_app.py
@@ -73,7 +73,7 @@ genai.configure(
     api_key=st.secrets["GEMINI_API_KEY"]
 )
 
-model = genai.GenerativeModel("gemini-2.0-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 # -----------------------------
 # SIDEBAR
